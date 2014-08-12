b9c8203aab089c27a48d11dfb1ecedb67ff41109
Fabien Meghazi <agr@amigrave.com>
2014-08-12 03:35:43 +0200

    Make it work on both python2 and python3 (Fixes #4)

--- git_qdiff.py
+++ git_qdiff.py
@@ -7,50 +7,68 @@ import sys
 class UnmetDependency(OSError):
     pass
 
+class CallError(Exception):
+    def __init__(self, *args, **kw):
+        returncode = kw.pop('returncode', 1)
+        super(CallError, self).__init__(*args, **kw)
+        self.returncode = returncode
+
+def call(*args):
+    try:
+        r = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        if e.output:
+            raise CallError(e.output.decode('utf8'), returncode=e.returncode)
+        raise e
+    return r.decode('utf8')
+
 def qdiff(left, right):
     left, right = opj(left, ''), opj(right, '')
     os.chdir(left)
-    subprocess.check_call(['bzr', 'init', '-q'])
-    subprocess.check_call(['bzr', 'add', '-q'])
-    subprocess.check_call(['bzr', 'commit', '-qmm'])
-    subprocess.check_call(['rsync', '-qavLt', '--delete', '--exclude=/.bzr', right, left])
-    subprocess.check_call(['bzr', 'add', '-q'])
-    subprocess.check_call(['bzr', 'qdiff', '-q'])
+    call('bzr', 'init', '-q')
+    call('bzr', 'add', '-q')
+    call('bzr', 'commit', '-qmm')
+    call('rsync', '-qavLt', '--delete', '--exclude=/.bzr', right, left)
+    call('bzr', 'add', '-q')
+    call('bzr', 'qdiff', '-q')
 
 def check_dependencies():
     try:
-        gitv = subprocess.check_output(['git', '--version']).split()[2]
-    except subprocess.CalledProcessError:
+        gitv = call('git', '--version').split()[2]
+    except CallError:
         raise UnmetDependency("Could not find `git` program")
 
-    ver = map(int, gitv.split('.')[0:3])
-    if cmp(ver, [1, 7, 11]) < 0:
+    ver = list(map(int, gitv.split('.')[0:3]))
+    if ver < [1, 7, 11]:
         raise UnmetDependency("Need at least `git` version 1.7.11+")
 
     try:
-        subprocess.check_output(['bzr', 'version']).splitlines()[0].split()[-1]
-    except subprocess.CalledProcessError:
+        call('bzr', 'version').splitlines()[0].split()[-1]
+    except CallError:
         raise UnmetDependency("Could not find `bzr` program")
 
     try:
-        subprocess.check_output(['bzr', 'qdiff', '--help'])
-    except subprocess.CalledProcessError:
+        call('bzr', 'qdiff', '--help')
+    except CallError:
         raise UnmetDependency("Could not find `bzr qdiff` plugin")
 
     try:
-        subprocess.check_output(['rsync', '--help'])
-    except subprocess.CalledProcessError:
+        call('rsync', '--help')
+    except CallError:
         raise UnmetDependency("Could not find `rsync` program")
 
 def main():
     try:
         check_dependencies()
-    except Exception, e:
-        sys.exit(e.message)
+    except Exception as e:
+        sys.exit(str(e))
     if len(sys.argv) == 3:
         left, right = sys.argv[1:3]
         if os.path.isdir(left) and os.path.isdir(right):
-            qdiff(left, right)
+            try:
+                qdiff(left, right)
+            except Exception as e:
+                sys.exit(str(e))
             sys.exit(0)
 
     # TODO: ensure infinite subcalls is not possible
@@ -58,5 +76,6 @@ def main():
     # print("Launching '%s'" % ' '.join(cmd))
     try:
         subprocess.check_call(cmd)
-    except subprocess.CalledProcessError, e:
-        sys.exit("git failed with returncode %d" % e.returncode)
+    except subprocess.CalledProcessError as e:
+        sys.stderr.write(str(e))
+        sys.exit(e.returncode)
